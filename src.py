697d4fe409b5d85c01650ace302d1f324e25612a
tzamou <72702965+tzamou@users.noreply.github.com>
2022-06-12 17:11:17 +0800

    Update src.py

--- src.py
+++ src.py
@@ -51,13 +51,14 @@ class Score:
 
 class BouncyBall_Game(gym.Env):
     def __init__(self):
+        # 建立視窗物件 以及視窗基本設定
         self.tk = Tk()
         self.tk.title(f'bouncy ballv2')
         self.tk.wm_attributes('-topmost', 1)
-        # self.tk.resizable(width=False, height=False)
+        self.tk.resizable(width=False, height=False)
         self.window_W = 640
         self.window_H = 800
-
+        #建立畫布 動畫都在畫布上進行
         self.canvas = Canvas(self.tk, width=self.window_W, height=self.window_H)
         self.canvas.pack()
         m = Middleline(self.canvas, self.window_W, self.window_H)
@@ -66,8 +67,7 @@ class BouncyBall_Game(gym.Env):
 
         self.platform_up = Platform(self.canvas, 'green', self.window_W / 2 - 50, 5)
         self.platform_down = Platform(self.canvas, 'red', self.window_W / 2 - 50, self.window_H - 15)
-        # self.canvas.create_text(50, self.window_H * 0.95, text=f'uping',font=('Arial', 15))
-
+        
         self.speed = 0.5
         self.atk = None
 
@@ -79,7 +79,7 @@ class BouncyBall_Game(gym.Env):
         self.ball_move_x = (np.random.random() * 2) - 1
         self.ball_move_y = 1
         self.ballpos = None
-
+        #強化學習相關資訊設定
         self.rewardlst = []
         self.avgreward = []
         self.combolst = []
@@ -89,16 +89,17 @@ class BouncyBall_Game(gym.Env):
         self.combo = 0
         self.done = False
 
-        # net
+        #客戶端訊息設定
         self.message = ''
         self.m_time = 0
+        
         self.tk.update()
 
-    def step(self, action, playing=False, act=''):
+    def step(self, action, playing=False, act=''):#每一步動作所要執行的內容
         self.done = False
-        self.edge_judge()
-        self.colliction_judge()
-        self.get_score()
+        self.edge_judge()#判斷球有沒有到最左邊最右邊的邊界
+        self.colliction_judge()#判斷球有沒有碰到平台
+        self.get_score()#判斷分數
         self.playing = playing
         if action == 0:
             up_move_x = 5
@@ -113,11 +114,12 @@ class BouncyBall_Game(gym.Env):
             up_move_x = -5
             down_move_x = -5
         self.ball.move(self.ball_move_x * self.speed, self.ball_move_y * self.speed)
-        # net
+        # 綁定按鍵有按下就會傳遞相應訊息至server端
         self.canvas.bind_all('<KeyPress-Right>', lambda event: self.send_to_server(event=event, message='Right'))
         self.canvas.bind_all('<KeyPress-Left>', lambda event: self.send_to_server(event=event, message='Left'))
         self.canvas.bind_all('<Key>', lambda event: self.send_to_server(event=event))
         # ---------------------------------------------------------------------------------
+        #判斷平台是否到邊界
         if self.platformpos_up[0] < 0:
             self.platform_up.move(1, 0)
         elif self.platformpos_up[2] > self.window_W:
@@ -125,6 +127,7 @@ class BouncyBall_Game(gym.Env):
         else:
             self.platform_up.move(up_move_x, 0)  # ;print(move_x)
         # ---------------------------------------------------------------------------------
+        # 根據自動或者手動來決定操控底下平台的方式
         if self.playing == False:
             try:
                 self.canvas.delete(self.t)
@@ -151,6 +154,7 @@ class BouncyBall_Game(gym.Env):
                 self.move_x = -1
                 self.platform_down.move(x=self.move_x, y=0)
         # ---------------------------------------------------------------------------------
+        #強化學習reward設定
         if self.platform_up.state == 'def':
             self.reward -= (abs((self.ballpos[0] + self.ballpos[2]) / 2 - (
                         self.platformpos_up[0] + self.platformpos_up[0]) / 2)) * 1e-6
@@ -165,8 +169,9 @@ class BouncyBall_Game(gym.Env):
         self.platformpos_down = self.canvas.coords(self.platform_down.id)
 
         self.rewardlst.append(self.reward)
-        # observation = np.array(self.ballpos + self.platformpos_up + self.platformpos_opponent)
+        #狀態空間設定
         observation = self.get_obs(self.ballpos, self.platformpos_up, self.platformpos_down)
+        
         if self.message != '':
             self.m_time += 1
             if self.m_time >= 50:
@@ -195,8 +200,6 @@ class BouncyBall_Game(gym.Env):
             self.done = True
 
     def colliction_judge(self):
-        # if self.ballpos[2] >= self.platformpos_up[0] and self.ballpos[0] <= self.platformpos_up[2]:
-        #     if self.ballpos[3] >= self.platformpos_up[1] and self.ballpos[1] <= self.platformpos_up[3]:
         ballpos_x = (self.ballpos[0] + self.ballpos[2]) / 2
         if self.ballpos[2] >= self.platformpos_up[0] and self.ballpos[0] <= self.platformpos_up[2]:
             if self.ballpos[1] <= self.platformpos_up[3]:
@@ -208,8 +211,6 @@ class BouncyBall_Game(gym.Env):
                 self.combo += 1;
                 print(f'ballpos:{self.ballpos},platformpos:{self.platformpos_up}')
 
-        # if self.ballpos[2] >= self.platformpos_down[0] and self.ballpos[0] <= self.platformpos_down[2]:
-        #     if self.ballpos[3] >= self.platformpos_down[1] and self.ballpos[1] <= self.platformpos_down[3]:
         if self.ballpos[2] >= self.platformpos_down[0] and self.ballpos[0] <= self.platformpos_down[2]:
             if self.ballpos[3] >= self.platformpos_down[1]:
                 self.platform_up.state = 'def'
@@ -227,7 +228,7 @@ class BouncyBall_Game(gym.Env):
         up_posy = (up_pos[1] + up_pos[3]) / 2
 
         down_posx = (down_pos[0] + down_pos[2]) / 2
-        down_posy = (down_pos[1] + down_pos[3]) / 2  # down
+        down_posy = (down_pos[1] + down_pos[3]) / 2 
 
         up_dis = np.linalg.norm(np.array([ballposx, ballposy]) - np.array([up_posx, up_posy]))
         down_dis = np.linalg.norm(np.array([ballposx, ballposy]) - np.array([down_posx, down_posy]))
@@ -239,10 +240,11 @@ class BouncyBall_Game(gym.Env):
             [up_posx, up_posy, down_posx, down_posy, ballposx, ballposy, up_dis, down_dis, upstate, downstate])
 
     def reset(self):
+        #得分後就重置整個遊戲(重新發球)
         self.canvas.delete(self.ball.id)
         m = Middleline(self.canvas, self.window_W, self.window_H)
         self.atk = np.random.choice(['up', 'down'])
-
+        #發球
         if self.atk == 'up':
             self.platformpos_up = self.canvas.coords(self.platform_up.id)
             self.platformpos_down = self.canvas.coords(self.platform_down.id)
